2904ae52b52c77f2fa4681377093529bc7c3585c
anhike <hoangtheanhhp@gmail.com>
2021-12-19 00:44:20 +0900

    fix front-end

--- components/common/content/index.tsx
+++ components/common/content/index.tsx
@@ -18,6 +18,7 @@ import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 export default function Example() {
+
   return (
     <>
       {/*
@@ -32,7 +33,6 @@ export default function Example() {
     </>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 content">
         <div className="max-w-md w-full space-y-8">
-          <form className="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div className="input-from">
@@ -70,11 +70,6 @@ export default function Example() {
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Reciever address"
                 />
-                <select className="selected-to">
-                  <option>CARA</option>
-                  <option>USDT</option>
-                  <option>DAI</option>
-                </select>
               </div>
             </div>
             <div>
@@ -85,7 +80,6 @@ export default function Example() {
                 Transfer
               </button>
             </div>
-          </form>
         </div>
       </div>
     </>
--- components/common/navbar/index.tsx
+++ components/common/navbar/index.tsx
@@ -106,30 +106,27 @@ export default function Example() {
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="#">
-              <span className="sr-only">Workflow</span>
-              <h1 className="font-bold">
-                
+              <h1 className="font-bold font-extrabold logo">
+                Carambola
               </h1>
             </a>
           </div>
           <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            <a href="#" className="text-base font-medium text-gray-1000 hover:text-gray-900">
+            <a href="#" className="text-base font-extrabold text-gray-900 hover:text-gray-900">
               Bridge
             </a>
-            <a href="#" className="text-base font-extrabold text-gray-900 hover:text-gray-900 logo">
-              Carambola
+            <a href="#" className="text-base font-medium text-gray-400 hover:text-gray-900">
+              Swap
             </a>
-            {/* <a href="#" className="text-base font-medium text-gray-400 hover:text-gray-900">
+            <a href="#" className="text-base font-medium text-gray-400 hover:text-gray-900">
               Pool
             </a>
             <a href="#" className="text-base font-medium text-gray-400 hover:text-gray-900">
               Vote
-            </a> */}
+            </a>
           </Popover.Group>
           <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-              <button onClick={disconnect}>Disconnect</button>
-            </a>
+            
             <a
               href="#"
               className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
