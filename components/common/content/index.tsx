1c713973e728f7d872642c6ef0a059cb885c67de
Ethan <ethanwalst69@gmail.com>
2021-12-18 22:53:04 +0700

    Rename receiver

--- components/common/content/index.tsx
+++ components/common/content/index.tsx
@@ -68,7 +68,7 @@ export default function Example() {
                   autoComplete="to-address"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Reciever address"
+                  placeholder="Receiver address"
                 />
                 <select className="selected-to">
                   <option>CARA</option>
